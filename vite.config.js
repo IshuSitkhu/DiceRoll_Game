7379cb34916e77f0ca81a5bebf3d33f6fb4f35ff
IshuSitikhu <ishusitikhu6@gmail.com>
2025-02-17 12:48:31 +0545

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",  // Make sure this is correct
+  base: './', // Ensure correct paths for assets
+  server: {
+    host: true, // Allow network access (optional)
+  }
 });
